f29ffb4de71db84007126ee3da019e9a74d53777
Shalette D'Souza <shalettdsouza@gmail.com>
2019-10-12 16:18:20 +0530

    Changes made

--- js/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', () {
-  parent =  document.getElementById('parallax');
-  children = parent.getElementsByTagName('img');
-  for(let i = 0; i < children.length; i++) {
-    children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
-  }
-})
